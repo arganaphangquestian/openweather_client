056b7171e4d6b1017a8e05f0e36757fec89378cd
Argana Phangquestian <arganaphangquestian@gmail.com>
2021-11-13 16:54:58 +0700

    remove api key

--- src/main/java/dev/arganaphang/service/WeatherService.java
+++ src/main/java/dev/arganaphang/service/WeatherService.java
@@ -5,6 +5,7 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface WeatherService {
-    @GET("onecall?lat=-6.21462&lon=106.845131&units=metric&exclude=minutely,hourly,alert,current&appid=78dc786c8d79f9719a00dbec32cb28b2")
+    REPLACE (APIKEY) with your API KEY
+    @GET("onecall?lat=-6.21462&lon=106.845131&units=metric&exclude=minutely,hourly,alert,current&appid=(APIKEY)")
     Call<WeatherResponse> getWeather();
 }
